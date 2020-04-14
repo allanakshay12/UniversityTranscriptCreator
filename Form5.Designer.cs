e411895b3362ac6476e696d6cdda400e2681f697
Jerry Allan Akshay <allanakshay@outlook.com>
2020-04-15 00:53:50 +0530

    Final Touches

--- Form5.Designer.cs
+++ Form5.Designer.cs
@@ -30,29 +30,29 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EnrollmentForm));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.AddSubjectName = new System.Windows.Forms.ComboBox();
+            this.AddCode = new System.Windows.Forms.ComboBox();
+            this.AddButton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.CodeModify = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.SubjectGroupBox = new System.Windows.Forms.GroupBox();
+            this.SubjectNameModify = new System.Windows.Forms.ComboBox();
+            this.DeleteSubject = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
             this.DataGridSubject = new System.Windows.Forms.DataGridView();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.SearchButton = new System.Windows.Forms.Button();
             this.SubjectNameView = new System.Windows.Forms.TextBox();
             this.label12 = new System.Windows.Forms.Label();
             this.CodeView = new System.Windows.Forms.TextBox();
             this.label11 = new System.Windows.Forms.Label();
-            this.AddCode = new System.Windows.Forms.ComboBox();
-            this.AddSubjectName = new System.Windows.Forms.ComboBox();
-            this.SubjectNameModify = new System.Windows.Forms.ComboBox();
-            this.CodeModify = new System.Windows.Forms.ComboBox();
-            this.SearchButton = new System.Windows.Forms.Button();
-            this.DeleteSubject = new System.Windows.Forms.Button();
-            this.CancelButton = new System.Windows.Forms.Button();
-            this.AddButton = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SubjectGroupBox.SuspendLayout();
@@ -75,6 +75,40 @@
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Enroll Student";
+            // 
+            // AddSubjectName
+            // 
+            this.AddSubjectName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.AddSubjectName.FormattingEnabled = true;
+            this.AddSubjectName.Location = new System.Drawing.Point(10, 99);
+            this.AddSubjectName.Name = "AddSubjectName";
+            this.AddSubjectName.Size = new System.Drawing.Size(204, 21);
+            this.AddSubjectName.TabIndex = 14;
+            // 
+            // AddCode
+            // 
+            this.AddCode.FormattingEnabled = true;
+            this.AddCode.Location = new System.Drawing.Point(10, 43);
+            this.AddCode.Name = "AddCode";
+            this.AddCode.Size = new System.Drawing.Size(204, 21);
+            this.AddCode.TabIndex = 13;
+            this.AddCode.SelectedIndexChanged += new System.EventHandler(this.AddCode_SelectedIndexChanged);
+            this.AddCode.TextUpdate += new System.EventHandler(this.AddCode_TextUpdate);
+            // 
+            // AddButton
+            // 
+            this.AddButton.BackColor = System.Drawing.Color.White;
+            this.AddButton.Image = ((System.Drawing.Image)(resources.GetObject("AddButton.Image")));
+            this.AddButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AddButton.Location = new System.Drawing.Point(10, 139);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Padding = new System.Windows.Forms.Padding(38, 0, 38, 0);
+            this.AddButton.Size = new System.Drawing.Size(204, 53);
+            this.AddButton.TabIndex = 12;
+            this.AddButton.Text = "Enroll Student";
+            this.AddButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.AddButton.UseVisualStyleBackColor = false;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
             // 
             // label2
             // 
@@ -107,6 +141,14 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Delete Enrollments";
             // 
+            // CodeModify
+            // 
+            this.CodeModify.FormattingEnabled = true;
+            this.CodeModify.Location = new System.Drawing.Point(10, 42);
+            this.CodeModify.Name = "CodeModify";
+            this.CodeModify.Size = new System.Drawing.Size(175, 21);
+            this.CodeModify.TabIndex = 5;
+            // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.White;
@@ -130,6 +172,45 @@
             this.SubjectGroupBox.TabIndex = 3;
             this.SubjectGroupBox.TabStop = false;
             this.SubjectGroupBox.Text = "Subject Details";
+            // 
+            // SubjectNameModify
+            // 
+            this.SubjectNameModify.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.SubjectNameModify.FormattingEnabled = true;
+            this.SubjectNameModify.Location = new System.Drawing.Point(6, 44);
+            this.SubjectNameModify.Name = "SubjectNameModify";
+            this.SubjectNameModify.Size = new System.Drawing.Size(243, 21);
+            this.SubjectNameModify.TabIndex = 17;
+            // 
+            // DeleteSubject
+            // 
+            this.DeleteSubject.BackColor = System.Drawing.Color.White;
+            this.DeleteSubject.Image = ((System.Drawing.Image)(resources.GetObject("DeleteSubject.Image")));
+            this.DeleteSubject.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.DeleteSubject.Location = new System.Drawing.Point(105, 80);
+            this.DeleteSubject.Name = "DeleteSubject";
+            this.DeleteSubject.Padding = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.DeleteSubject.Size = new System.Drawing.Size(144, 38);
+            this.DeleteSubject.TabIndex = 16;
+            this.DeleteSubject.Text = "Delete Enrollment";
+            this.DeleteSubject.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.DeleteSubject.UseVisualStyleBackColor = false;
+            this.DeleteSubject.Click += new System.EventHandler(this.DeleteSubject_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.BackColor = System.Drawing.Color.White;
+            this.CancelButton.Image = ((System.Drawing.Image)(resources.GetObject("CancelButton.Image")));
+            this.CancelButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.CancelButton.Location = new System.Drawing.Point(6, 80);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Padding = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.CancelButton.Size = new System.Drawing.Size(93, 38);
+            this.CancelButton.TabIndex = 15;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.CancelButton.UseVisualStyleBackColor = false;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
             // 
             // label7
             // 
@@ -158,7 +239,7 @@
             this.groupBox3.Size = new System.Drawing.Size(506, 186);
             this.groupBox3.TabIndex = 4;
             this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "View Subject Details";
+            this.groupBox3.Text = "View Enrollment Details";
             // 
             // groupBox5
             // 
@@ -196,6 +277,21 @@
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Query Parameters";
             // 
+            // SearchButton
+            // 
+            this.SearchButton.BackColor = System.Drawing.Color.White;
+            this.SearchButton.Image = ((System.Drawing.Image)(resources.GetObject("SearchButton.Image")));
+            this.SearchButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.SearchButton.Location = new System.Drawing.Point(6, 100);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Padding = new System.Windows.Forms.Padding(37, 0, 37, 0);
+            this.SearchButton.Size = new System.Drawing.Size(155, 49);
+            this.SearchButton.TabIndex = 17;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.SearchButton.UseVisualStyleBackColor = false;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
             // SubjectNameView
             // 
             this.SubjectNameView.Location = new System.Drawing.Point(6, 74);
@@ -227,102 +323,6 @@
             this.label11.Size = new System.Drawing.Size(146, 13);
             this.label11.TabIndex = 2;
             this.label11.Text = "Student Registration Number:";
-            // 
-            // AddCode
-            // 
-            this.AddCode.FormattingEnabled = true;
-            this.AddCode.Location = new System.Drawing.Point(10, 43);
-            this.AddCode.Name = "AddCode";
-            this.AddCode.Size = new System.Drawing.Size(204, 21);
-            this.AddCode.TabIndex = 13;
-            this.AddCode.SelectedIndexChanged += new System.EventHandler(this.AddCode_SelectedIndexChanged);
-            this.AddCode.TextUpdate += new System.EventHandler(this.AddCode_TextUpdate);
-            // 
-            // AddSubjectName
-            // 
-            this.AddSubjectName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.AddSubjectName.FormattingEnabled = true;
-            this.AddSubjectName.Location = new System.Drawing.Point(10, 99);
-            this.AddSubjectName.Name = "AddSubjectName";
-            this.AddSubjectName.Size = new System.Drawing.Size(204, 21);
-            this.AddSubjectName.TabIndex = 14;
-            // 
-            // SubjectNameModify
-            // 
-            this.SubjectNameModify.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.SubjectNameModify.FormattingEnabled = true;
-            this.SubjectNameModify.Location = new System.Drawing.Point(6, 44);
-            this.SubjectNameModify.Name = "SubjectNameModify";
-            this.SubjectNameModify.Size = new System.Drawing.Size(243, 21);
-            this.SubjectNameModify.TabIndex = 17;
-            // 
-            // CodeModify
-            // 
-            this.CodeModify.FormattingEnabled = true;
-            this.CodeModify.Location = new System.Drawing.Point(10, 42);
-            this.CodeModify.Name = "CodeModify";
-            this.CodeModify.Size = new System.Drawing.Size(175, 21);
-            this.CodeModify.TabIndex = 5;
-            // 
-            // SearchButton
-            // 
-            this.SearchButton.BackColor = System.Drawing.Color.White;
-            this.SearchButton.Image = ((System.Drawing.Image)(resources.GetObject("SearchButton.Image")));
-            this.SearchButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.SearchButton.Location = new System.Drawing.Point(6, 100);
-            this.SearchButton.Name = "SearchButton";
-            this.SearchButton.Padding = new System.Windows.Forms.Padding(37, 0, 37, 0);
-            this.SearchButton.Size = new System.Drawing.Size(155, 49);
-            this.SearchButton.TabIndex = 17;
-            this.SearchButton.Text = "Search";
-            this.SearchButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.SearchButton.UseVisualStyleBackColor = false;
-            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
-            // 
-            // DeleteSubject
-            // 
-            this.DeleteSubject.BackColor = System.Drawing.Color.White;
-            this.DeleteSubject.Image = ((System.Drawing.Image)(resources.GetObject("DeleteSubject.Image")));
-            this.DeleteSubject.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.DeleteSubject.Location = new System.Drawing.Point(105, 80);
-            this.DeleteSubject.Name = "DeleteSubject";
-            this.DeleteSubject.Padding = new System.Windows.Forms.Padding(8, 0, 8, 0);
-            this.DeleteSubject.Size = new System.Drawing.Size(144, 38);
-            this.DeleteSubject.TabIndex = 16;
-            this.DeleteSubject.Text = "Delete Enrollment";
-            this.DeleteSubject.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.DeleteSubject.UseVisualStyleBackColor = false;
-            this.DeleteSubject.Click += new System.EventHandler(this.DeleteSubject_Click);
-            // 
-            // CancelButton
-            // 
-            this.CancelButton.BackColor = System.Drawing.Color.White;
-            this.CancelButton.Image = ((System.Drawing.Image)(resources.GetObject("CancelButton.Image")));
-            this.CancelButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.CancelButton.Location = new System.Drawing.Point(6, 80);
-            this.CancelButton.Name = "CancelButton";
-            this.CancelButton.Padding = new System.Windows.Forms.Padding(7, 0, 7, 0);
-            this.CancelButton.Size = new System.Drawing.Size(93, 38);
-            this.CancelButton.TabIndex = 15;
-            this.CancelButton.Text = "Cancel";
-            this.CancelButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.CancelButton.UseVisualStyleBackColor = false;
-            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
-            // 
-            // AddButton
-            // 
-            this.AddButton.BackColor = System.Drawing.Color.White;
-            this.AddButton.Image = ((System.Drawing.Image)(resources.GetObject("AddButton.Image")));
-            this.AddButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.AddButton.Location = new System.Drawing.Point(10, 139);
-            this.AddButton.Name = "AddButton";
-            this.AddButton.Padding = new System.Windows.Forms.Padding(38, 0, 38, 0);
-            this.AddButton.Size = new System.Drawing.Size(204, 53);
-            this.AddButton.TabIndex = 12;
-            this.AddButton.Text = "Enroll Student";
-            this.AddButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.AddButton.UseVisualStyleBackColor = false;
-            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
             // 
             // EnrollmentForm
             // 
